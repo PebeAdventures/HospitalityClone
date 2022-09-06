c9d2ffb1df5ba390aceb6b98cd61c8feb1a70278
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-09-06 14:05:25 +0200

    moved project to src

---
+++ src/Hospitality.Government.Insurance.API/Controllers/InsuranceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospitality.Government.Insurance.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InsuranceController : ControllerBase
+    {
+        private Random random;
+        public InsuranceController()
+            => random = new Random();
+
+        [HttpPost]
+        public async Task<IActionResult> CheckIfIsured(int idOfPerson)
+            => Ok(random.Next(0, 1));
+    }
+}
