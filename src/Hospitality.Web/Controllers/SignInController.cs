a4ca2fa500c678bf7e81fe94496dbfb4535dc3bf
Nadzeya Sivukha (CODECOOL POLAND SP. Z O.O.) <WGK867@motorolasolutions.com>
2022-09-06 14:48:51 +0200

    Logo added

---
+++ src/Hospitality.Web/Controllers/SignInController.cs
@@ -0,0 +1,15 @@
+﻿using Hospitality.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace Hospitality.Web.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
