61c9d35b0568274837f75637dadaaa4df63f0931
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-09-06 13:23:13 +0200

    implemented IdentityController class

--- src/Hospitality.Identity.API/Controllers/IdentityController.cs
+++ src/Hospitality.Identity.API/Controllers/IdentityController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SecondExam.Services.Services.Auth;
 
 namespace Hospitality.Identity.API.Controllers
 {
@@ -7,5 +8,12 @@ namespace Hospitality.Identity.API.Controllers
     [ApiController]
     public class IdentityController : ControllerBase
     {
+        private LogInService LogInService;
+        public IdentityController(LogInService logInService)
+            => LogInService = logInService;
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+            => Ok(LogInService.Login(email, password));
     }
 }
---
+++ src/Hospitality.Identity.API/Services/LogInService.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SecondExam.Services.Services.Auth
+{
+    public class LogInService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public LogInService(IConfiguration configuration, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+        public async Task<string?> Login(string email, string password)
+        {
+            var existingUser = await _userManager.FindByEmailAsync(email);
+            if (existingUser == null) return null;
+            var isCorrect = await _userManager.CheckPasswordAsync(existingUser, password);
+            if (!isCorrect) return null;
+            return await GenerateJwtToken(existingUser);
+        }
+        public async Task<string> GenerateJwtToken(IdentityUser user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var claims = await GetValidClaims(user);
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddHours(8),
+                signingCredentials: signIn);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        public async Task<List<Claim>> GetValidClaims(IdentityUser user)
+        {
+            IdentityOptions _options = new IdentityOptions();
+            var claims = new List<Claim>
+            {
+                new Claim("Id", user.Id),
+                new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("UserId", user.Id.ToString()),
+            };
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(userClaims);
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, userRole));
+                var role = await _roleManager.FindByNameAsync(userRole);
+                if (role != null)
+                {
+                    var roleClaims = await _roleManager.GetClaimsAsync(role);
+                    foreach (Claim roleClaim in roleClaims)
+                    {
+                        claims.Add(roleClaim);
+                    }
+                }
+            }
+            return claims;
+        }
+    }
+}
