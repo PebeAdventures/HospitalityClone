876164ce4ad176e8c8201bcb132706a6b408f07c
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-09-06 13:40:02 +0200

    Add new CheckUp API microservice and new CheckUpController

---
+++ Hospitality.CheckUp.API/Controllers/CheckUpController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hospitality.CheckUp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckUpController : ControllerBase
+    {
+
+
+        // POST api/<CheckUpController>
+        [HttpPost]
+        public void AddNewCheckUp()
+        {
+
+        }
+
+    }
+}
