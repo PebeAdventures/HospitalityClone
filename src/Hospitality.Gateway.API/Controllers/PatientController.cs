7bee08a3aef7a9ea7e2242c3e3b02d52301242f6
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-09-07 11:34:30 +0200

    added 'async' words to names of methods

--- src/Hospitality.Gateway.API/Controllers/PatientController.cs
+++ src/Hospitality.Gateway.API/Controllers/PatientController.cs
@@ -10,31 +10,13 @@ namespace Hospitality.Gateway.API.Controllers
     public class PatientController : ControllerBase
     {
         private HttpClient _httpClient;
-
         public PatientController(IHttpClientFactory httpClientFactory)
              => _httpClient = httpClientFactory.CreateClient();
-        //Checkup:
-
-        //POST:  Create new Checkup
-
-        //Examination:
-
-        //POST(dodawanie exmination do pacjenta)
-        //GET(examination Type List)
-        //GET(Get Current Patient Examination's results)
-
-        //Identity:
-
-        //POST: log in
-
-        //Patient:
-
-        //POST(register new patient - add)
-
+        
         [HttpPost]
-        public async Task<IActionResult> RegisterNewPatient(object newPatient)
-            => await GetContent(newPatient, ""); // LINK DO UZUPEŁNIENIA !!! 
-        private async Task<IActionResult> GetContent(object newPatient, string url)
+        public async Task<IActionResult> RegisterNewPatientAsync(object newPatient)
+            => await GetContentAsync(newPatient, ""); // LINK DO UZUPEŁNIENIA !!! 
+        private async Task<IActionResult> GetContentAsync(object newPatient, string url)
         {
             var json = JsonConvert.SerializeObject(newPatient);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
