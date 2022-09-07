bc6f8bd8cae22cc73f8d4406d5df7b194db44339
Nadzeya Sivukha (CODECOOL POLAND SP. Z O.O.) <WGK867@motorolasolutions.com>
2022-09-08 01:14:43 +0200

    TestResultView

---
+++ src/Hospitality.Web/Controllers/TestResultController.cs
@@ -0,0 +1,24 @@
+﻿using Hospitality.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospitality.Web.Controllers
+{
+    public class TestResultController : Controller
+    {
+        private List<TestResultClass> results;
+
+        public TestResultController()
+        {
+            results = new List<TestResultClass>() {
+                new TestResultClass(){ name = "Badanie krwi", result = "niski poziom hemoglobiny", price = 50},
+                new TestResultClass(){ name = "USG serca", result = "bez patalogii", price = 300},
+                new TestResultClass(){ name = "Test na alergeny", result = "pozytywny na brzoze", price = 250}
+            };
+        }
+
+        public IActionResult TestResult()
+        {
+            return View(results);
+        }
+    }
+}
---
+++ src/Hospitality.Web/Models/TestResultClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospitality.Web.Models
+{
+    public class TestResultClass
+    {
+        public string name { get; set; }
+        public string result { get; set; }
+        public int price { get; set; }
+    }
+}
