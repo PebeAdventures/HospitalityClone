4db68933c434b10845c417e3934c671f5f4fcee8
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-09-09 09:40:55 +0200

    refactored checkup controller, now it is possible to pass pesel of patient

--- src/Hospitality.Web/Controllers/CheckUpController.cs
+++ src/Hospitality.Web/Controllers/CheckUpController.cs
@@ -13,51 +13,40 @@ namespace Hospitality.Web.Controllers
 
     public class CheckUpController : Controller
     {
-
+        private int pesel;
         private HttpClient _httpClient;
         public CheckUpController(IHttpClientFactory httpClientFactory)
              => _httpClient = httpClientFactory.CreateClient();
-        private async Task<IActionResult> GetContentAsync(NewCheckUpDTO newCheckup, string url)
+        
+        [HttpGet]
+        public IActionResult CheckUp(string peselInString)
         {
-            var json = JsonConvert.SerializeObject(newCheckup);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            //var token = HttpContext.Session.GetString("token");
-            var kupa = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
-            _httpClient.DefaultRequestHeaders.Authorization = kupa;
-            var response = await _httpClient.PostAsync(url, content);
-            if (!response.IsSuccessStatusCode || response is null) return StatusCode(404);
-            return StatusCode(201);
-        }
-
-
-        public IActionResult CheckUp(PatientDataForStartVisit patientDataForStartVisit)
-        {
-            string patientID = "";
-            if (patientDataForStartVisit.PatientPesel != null)
-            {
-               // patientID = Int32.Parse(patientDataForStartVisit.PatientPesel);
-            }
-            TempData["patientId"] = patientDataForStartVisit.PatientPesel;
+            int.TryParse(peselInString, out pesel);
             return View();
         }
-
 
         [HttpPost]
         public async Task<IActionResult> CheckUp(NewCheckUpDTO newCheckUpDTO)
         {
-            //newCheckUpDTO.IdPatient = (int)TempData["patientId"];
-            newCheckUpDTO.IdPatient = 123;
+            // dopisać request do patient api o Id pacjęta
+            //newCheckUpDTO.IdPatient = 
+            newCheckUpDTO.IdPatient = 1; // - to do zmiany
             if (!ModelState.IsValid)
             {
                 return View(newCheckUpDTO);
             }
             var json = JsonConvert.SerializeObject(newCheckUpDTO);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
             await GetContentAsync(newCheckUpDTO, "https://localhost:7236/api/CheckUp");
-            //await _httpClient.PostAsync("https://localhost:7236/api/CheckUp", content);
-
             return RedirectToAction("Index", "Home", null);
+        }
+        private async Task<IActionResult> GetContentAsync(NewCheckUpDTO newCheckup, string url)
+        {
+            var json = JsonConvert.SerializeObject(newCheckup);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
+            var response = await _httpClient.PostAsync(url, content);
+            if (!response.IsSuccessStatusCode || response is null) return StatusCode(404);
+            return StatusCode(201);
         }
     }
 }
