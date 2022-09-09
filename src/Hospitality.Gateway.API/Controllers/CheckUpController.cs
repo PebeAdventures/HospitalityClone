c45b66e951d1e95ac5731bd4b4a3acf65465615e
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-09-09 13:45:26 +0200

    WORKING VERSION - CONNECT BETWEEN CHECKUP AND MVC

--- src/Hospitality.Gateway.API/Controllers/CheckUpController.cs
+++ src/Hospitality.Gateway.API/Controllers/CheckUpController.cs
@@ -30,3 +30,36 @@ namespace Hospitality.Gateway.API.Controllers
         }
     }
 }
+
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json;
+//using System.Text;
+//using Hospitality.Common.DTO.CheckUp;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using System.Net.Http.Headers;
+
+//namespace Hospitality.Gateway.API.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class CheckUpController : ControllerBase
+//    {
+//        private HttpClient _httpClient;
+//        public CheckUpController(IHttpClientFactory httpClientFactory)
+//             => _httpClient = httpClientFactory.CreateClient();
+
+//        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Doctor")]
+//        [HttpPost]
+//        public async Task<IActionResult> CreateNewCheckupAsync(NewCheckUpDTO newCheckup)
+//            => Ok(await GetContentAsync(newCheckup, "https://localhost:7280/api/CheckUp")); // LINK DO UZUPEŁNIENIA !!! 
+//        private async Task<HttpResponseMessage> GetContentAsync(NewCheckUpDTO newCheckup, string url)
+//        {
+//            var json = JsonConvert.SerializeObject(newCheckup);
+//            var content = new StringContent(json, Encoding.UTF8, "application/json");
+//            var response = await _httpClient.PostAsync(url, content);
+//            return response;
+//        }
+//    }
+//}
--- src/Hospitality.Web/Controllers/CheckUpController.cs
+++ src/Hospitality.Web/Controllers/CheckUpController.cs
@@ -4,10 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Text.Json;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Hospitality.Common.DTO.Patient;
 
 namespace Hospitality.Web.Controllers
 {
@@ -15,45 +13,29 @@ namespace Hospitality.Web.Controllers
 
     public class CheckUpController : Controller
     {
-        private string pesel;
+
         private HttpClient _httpClient;
         public CheckUpController(IHttpClientFactory httpClientFactory)
              => _httpClient = httpClientFactory.CreateClient();
-        
-        [HttpGet]
-        public IActionResult CheckUp(string peselInString)
+        public IActionResult CheckUp(PatientDataForStartVisit patientDataForStartVisit)
         {
-            //TempData["pesel"] = peselInString;
-            return View(new NewCheckUpDTO() { PeselOfPatient = peselInString});
+            string patientID = "";
+            if (patientDataForStartVisit.PatientPesel != null)
+            {
+                // patientID = Int32.Parse(patientDataForStartVisit.PatientPesel);
+            }
+            TempData["patientId"] = patientDataForStartVisit.PatientPesel;
+            return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> CheckUp(NewCheckUpDTO newCheckUpDTO)
         {
-            //var idOfPatient = await GetIdOfPatient(newCheckUpDTO.PeselOfPatient);
-            //if (idOfPatient == 0)
-            //{
-            //    return RedirectToAction("StartVisit", "StartVisit");
-            //}
-            //newCheckUpDTO.IdPatient = idOfPatient;
-            newCheckUpDTO.IdPatient = 1;
-            if (!ModelState.IsValid)
-            {
-                return View(newCheckUpDTO);
-            }
-            var json = JsonConvert.SerializeObject(newCheckUpDTO);
+            newCheckUpDTO.IdDoctor = 1;
+            newCheckUpDTO.PeselOfPatient = "dupa";
+            newCheckUpDTO.IdPatient = 123;
             await GetContentAsync(newCheckUpDTO, "https://localhost:7236/api/CheckUp");
             return RedirectToAction("Index", "Home", null);
-        }
-        private async Task<int> GetIdOfPatient(string pesel)
-        {
-            var json = JsonConvert.SerializeObject(pesel);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
-            var response = await _httpClient.PostAsync($"https://localhost:7236/api/Patient?pesel={pesel}", content);
-            var dupa = System.Text.Json.JsonSerializer.Deserialize<PatientDoctorViewDTO>(await response.Content.ReadAsStringAsync());
-            if (dupa is null) return 0;
-            return dupa.HospitalPatientId;
         }
         private async Task<IActionResult> GetContentAsync(NewCheckUpDTO newCheckup, string url)
         {
@@ -66,3 +48,81 @@ namespace Hospitality.Web.Controllers
         }
     }
 }
+
+//using Hospitality.Common.DTO.CheckUp;
+//using Hospitality.Web.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json;
+//using System.Net.Http.Headers;
+//using System.Text;
+//using System.Text.Json;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.AspNetCore.Authorization;
+//using Hospitality.Common.DTO.Patient;
+
+//namespace Hospitality.Web.Controllers
+//{
+//    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Doctor")]
+
+//    public class CheckUpController : Controller
+//    {
+//        private string pesel;
+//        private HttpClient _httpClient;
+//        public CheckUpController(IHttpClientFactory httpClientFactory)
+//             => _httpClient = httpClientFactory.CreateClient();
+
+//        [HttpGet]
+//        public IActionResult CheckUp(string peselInString)
+//        {
+//            //TempData["pesel"] = peselInString;
+//            return View(new NewCheckUpDTO() { PeselOfPatient = peselInString});
+//        }
+
+//        [HttpPost]
+//        public async Task<IActionResult> CheckUp(NewCheckUpDTO newCheckUpDTO)
+//        {
+//            newCheckUpDTO.IdPatient = 1;
+//            var json = JsonConvert.SerializeObject(newCheckUpDTO);
+//            var content = new StringContent(json, Encoding.UTF8, "application/json");
+//            var token = HttpContext.Session.GetString("token");
+//            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+//            var response = await _httpClient.PostAsync("https://localhost:7236/api/CheckUp", content);
+//            if (response.Content != null && response.IsSuccessStatusCode)
+//                return RedirectToAction("Index", "Home", null);
+//            return View(newCheckUpDTO);
+
+//            //var idOfPatient = await GetIdOfPatient(newCheckUpDTO.PeselOfPatient);
+//            //if (idOfPatient == 0)
+//            //{
+//            //    return RedirectToAction("StartVisit", "StartVisit");
+//            //}
+//            //newCheckUpDTO.IdPatient = idOfPatient;
+
+//            //    if (!ModelState.IsValid)
+//            //    {
+//            //        return View(newCheckUpDTO);
+//            //    }
+//            //    var json = JsonConvert.SerializeObject(newCheckUpDTO);
+//            //    await GetContentAsync(newCheckUpDTO, "https://localhost:7236/api/CheckUp");
+//        }
+//        private async Task<int> GetIdOfPatient(string pesel)
+//        {
+//            var json = JsonConvert.SerializeObject(pesel);
+//            var content = new StringContent(json, Encoding.UTF8, "application/json");
+//            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
+//            var response = await _httpClient.PostAsync($"https://localhost:7236/api/Patient?pesel={pesel}", content);
+//            var dupa = System.Text.Json.JsonSerializer.Deserialize<PatientDoctorViewDTO>(await response.Content.ReadAsStringAsync());
+//            if (dupa is null) return 0;
+//            return dupa.HospitalPatientId;
+//        }
+//        private async Task<IActionResult> GetContentAsync(NewCheckUpDTO newCheckup, string url)
+//        {
+//            var json = JsonConvert.SerializeObject(newCheckup);
+//            var content = new StringContent(json, Encoding.UTF8, "application/json");
+//            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
+//            var response = await _httpClient.PostAsync(url, content);
+//            if (!response.IsSuccessStatusCode || response is null) return StatusCode(404);
+//            return StatusCode(201);
+//        }
+//    }
+//}
