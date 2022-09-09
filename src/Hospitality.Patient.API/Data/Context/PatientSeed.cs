b8c491e069041e06d01dd337802b17fe00173050
Artgelika <artgelika.com>
2022-09-09 09:53:00 +0200

    Add patient record into database

--- src/Hospitality.Patient.API/Data/Context/PatientSeed.cs
+++ src/Hospitality.Patient.API/Data/Context/PatientSeed.cs
@@ -7,7 +7,7 @@
             List<HospitalPatient> patients = new();
             patients.Add(new HospitalPatient()
             {
-                HospitalPatientId = 1,
+                HospitalPatientId = 5,
                 PatientName = "Aniela",
                 PatientSurname = "Nowak",
                 PatientPesel = "99112234543",
@@ -16,7 +16,34 @@
                 Email = "aniela.nowak@gmail.com",
                 PhoneNumber = "213769420",
                 IsInsured = true,
-            }); ;
+            }); 
+            
+            patients.Add(new HospitalPatient()
+            {
+                HospitalPatientId = 6,
+                PatientName = "Ania",
+                PatientSurname = "Okrasa",
+                PatientPesel = "98112234543",
+                BirthDate = new DateTime(1998, 11, 22),
+                Address = "Jaworowa",
+                Email = "ania.okrasa@gmail.com",
+                PhoneNumber = "123456456",
+                IsInsured = true,
+            }); 
+
+            patients.Add(new HospitalPatient()
+            {
+                HospitalPatientId = 7,
+                PatientName = "Michał",
+                PatientSurname = "Jakos",
+                PatientPesel = "97112234543",
+                BirthDate = new DateTime(1997, 11, 22),
+                Address = "Fiołkowa",
+                Email = "michal.jakos@gmail.com",
+                PhoneNumber = "456789123",
+                IsInsured = true,
+            }); 
+
             builder.Entity<HospitalPatient>().HasData(patients);
         }
     }
