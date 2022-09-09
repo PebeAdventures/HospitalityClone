3c2f626332a0bdad14a853cb29ad4d29f4363a81
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-09-09 11:32:41 +0200

    dupa

--- src/Hospitality.Web/Controllers/CheckUpController.cs
+++ src/Hospitality.Web/Controllers/CheckUpController.cs
@@ -4,10 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Text.Json;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Hospitality.Common.DTO.Patient;
 
 namespace Hospitality.Web.Controllers
 {
@@ -15,51 +13,44 @@ namespace Hospitality.Web.Controllers
 
     public class CheckUpController : Controller
     {
-        private string pesel;
+
         private HttpClient _httpClient;
         public CheckUpController(IHttpClientFactory httpClientFactory)
              => _httpClient = httpClientFactory.CreateClient();
-        
-        [HttpGet]
-        public IActionResult CheckUp(string peselInString)
+       
+
+
+        public IActionResult CheckUp(PatientDataForStartVisit patientDataForStartVisit)
         {
-            //TempData["pesel"] = peselInString;
-            return View(new NewCheckUpDTO() { PeselOfPatient = peselInString});
+            string patientID = "";
+            if (patientDataForStartVisit.PatientPesel != null)
+            {
+               // patientID = Int32.Parse(patientDataForStartVisit.PatientPesel);
+            }
+            TempData["patientId"] = patientDataForStartVisit.PatientPesel;
+            return View();
         }
+
 
         [HttpPost]
         public async Task<IActionResult> CheckUp(NewCheckUpDTO newCheckUpDTO)
         {
-            //var idOfPatient = await GetIdOfPatient(newCheckUpDTO.PeselOfPatient);
-            //if (idOfPatient == 0)
-            //{
-            //    return RedirectToAction("StartVisit", "StartVisit");
-            //}
-            //newCheckUpDTO.IdPatient = idOfPatient;
-            newCheckUpDTO.IdPatient = 1;
-            if (!ModelState.IsValid)
-            {
-                return View(newCheckUpDTO);
-            }
+            //newCheckUpDTO.IdPatient = (int)TempData["patientId"];
+            newCheckUpDTO.IdPatient = 123;
+            
             var json = JsonConvert.SerializeObject(newCheckUpDTO);
-            await GetContentAsync(newCheckUpDTO, "https://localhost:7236/api/CheckUp");
-            return RedirectToAction("Index", "Home", null);
-        }
-        private async Task<int> GetIdOfPatient(string pesel)
-        {
-            var json = JsonConvert.SerializeObject(pesel);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
-            var response = await _httpClient.PostAsync($"https://localhost:7236/api/Patient?pesel={pesel}", content);
-            var dupa = System.Text.Json.JsonSerializer.Deserialize<PatientDoctorViewDTO>(await response.Content.ReadAsStringAsync());
-            if (dupa is null) return 0;
-            return dupa.HospitalPatientId;
+            await GetContentAsync(newCheckUpDTO, "https://localhost:7236/api/CheckUp");
+
+            return RedirectToAction("Index", "Home", null);
         }
         private async Task<IActionResult> GetContentAsync(NewCheckUpDTO newCheckup, string url)
         {
             var json = JsonConvert.SerializeObject(newCheckup);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
+            var token = HttpContext.Session.GetString("token");
+            var kupa = new AuthenticationHeaderValue("Bearer", token);
+            _httpClient.DefaultRequestHeaders.Authorization = kupa;
             var response = await _httpClient.PostAsync(url, content);
             if (!response.IsSuccessStatusCode || response is null) return StatusCode(404);
             return StatusCode(201);
