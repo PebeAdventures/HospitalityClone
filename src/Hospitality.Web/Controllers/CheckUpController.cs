d2fee5241444ac515d00327a69999a7efb8332bf
Tomek Gawron <tomekgawron621@gmail.com>
2022-09-19 10:44:07 +0200

    Add necessary using

--- src/Hospitality.Web/Controllers/CheckUpController.cs
+++ src/Hospitality.Web/Controllers/CheckUpController.cs
@@ -6,14 +6,15 @@ using System.Net.Http.Headers;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Hospitality.Common.DTO.Patient;
 
 namespace Hospitality.Web.Controllers
 {
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Doctor")]
-
     public class CheckUpController : Controller
     {
         private HttpClient _httpClient;
+
         public CheckUpController(IHttpClientFactory httpClientFactory)
              => _httpClient = httpClientFactory.CreateClient();
 
@@ -23,12 +24,11 @@ namespace Hospitality.Web.Controllers
             var idOfPatient = await GetIdOfPatient($"https://localhost:7236/api/Patient?pesel={patientDataForStartVisit.PatientPesel}");
             if (idOfPatient != 0)
             {
-                var newCheckUpDTO = new NewCheckUpDTO {PeselOfPatient = patientDataForStartVisit.PatientPesel, IdPatient = idOfPatient};
+                var newCheckUpDTO = new NewCheckUpDTO { PeselOfPatient = patientDataForStartVisit.PatientPesel, IdPatient = idOfPatient };
                 return View(newCheckUpDTO);
             }
             return RedirectToAction("StartVisit", "StartVisit", patientDataForStartVisit);
         }
-
 
         [HttpPost]
         public async Task<IActionResult> CheckUp(NewCheckUpDTO newCheckUpDTO)
@@ -37,15 +37,17 @@ namespace Hospitality.Web.Controllers
             await SaveNewCheckupAsync(newCheckUpDTO, "https://localhost:7236/api/CheckUp");
             return RedirectToAction("Index", "Home", null);
         }
+
         private async Task<int> GetIdOfPatient(string url)
         {
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
             var response = await _httpClient.GetAsync(url);
             if (!response.IsSuccessStatusCode || response is null) return 0;
-            var patientDoctorViewDTO = JsonConvert.DeserializeObject<PatientDoctorViewDTO>(await response.Content.ReadAsStringAsync()); 
+            var patientDoctorViewDTO = JsonConvert.DeserializeObject<PatientDoctorViewDTO>(await response.Content.ReadAsStringAsync());
             if (patientDoctorViewDTO is null) return 0;
             return patientDoctorViewDTO.HospitalPatientId;
         }
+
         private async Task<IActionResult> SaveNewCheckupAsync(NewCheckUpDTO newCheckup, string url)
         {
             var json = JsonConvert.SerializeObject(newCheckup);
