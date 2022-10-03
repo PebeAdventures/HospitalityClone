6d66b3876b7e845ba0e66fb83601cca1919284aa
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-10-03 16:16:31 +0200

    add DTO to send/recive data from gateway

---
+++ src/Hospitality.Common/DTO/Temperature/PatientTemperaturesViewDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospitality.Common.DTO.Temperature
+{
+    public class PatientTemperaturesViewDTO
+    {
+
+        public string PatientId { get; set; }
+        public decimal Temperature { get; set; }
+        public DateTime MeasurementDate { get; set; }
+    }
+}
