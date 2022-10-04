1607ab0c790583168af622c7db3f11180898da74
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-10-04 11:40:43 +0200

    Add Temperature Service in View and Controller with base read method

---
+++ src/Hospitality.Web/Controllers/TemperatureController.cs
@@ -0,0 +1,38 @@
+﻿using Hospitality.Common.DTO.Examination;
+using Hospitality.Common.DTO.Temperature;
+using Hospitality.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Hospitality.Web.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Doctor")]
+    public class TemperatureController : Controller
+    {
+        private HttpClient _httpClient;
+        private List<PatientTemperaturesViewDTO> patientTemperaturesViewDTO;
+        private ITemperatureService _temperatureService;
+
+        public TemperatureController(IHttpClientFactory httpClientFactory, ITemperatureService temperatureService)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            patientTemperaturesViewDTO = new List<PatientTemperaturesViewDTO>();
+            _temperatureService = temperatureService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Temperature(string patientId)
+        {
+            var patientTemperatures = await _temperatureService.GetPatientTemperatures(patientId, HttpContext.Session.GetString("token"));
+            if (patientTemperatures != null)
+            {
+                return View(patientTemperatures);
+            }
+            patientTemperatures = new List<PatientTemperaturesViewDTO>() { new PatientTemperaturesViewDTO() { PatientId = patientId, Temperature = 0, MeasurementDate = DateTime.Now } };
+            return View(patientTemperatures);
+        }
+    }
+}
---
+++ src/Hospitality.Web/Services/TemperatureService.cs
@@ -0,0 +1,35 @@
+﻿using Hospitality.Common.DTO.Examination;
+using Hospitality.Common.DTO.Temperature;
+using Hospitality.Web.Services.Interfaces;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Hospitality.Web.Services
+{
+    public class TemperatureService : ITemperatureService
+    {
+
+
+        private HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public TemperatureService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration;
+            _httpClient = httpClientFactory.CreateClient();
+
+        }
+
+        public async Task<List<PatientTemperaturesViewDTO>> GetPatientTemperatures(string patientId, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync(_configuration["Paths:PatientTemperaturesList"] + patientId);
+            if (!response.IsSuccessStatusCode || response is null) return null;
+            List<PatientTemperaturesViewDTO> temperatureInfo = JsonConvert.DeserializeObject<List<PatientTemperaturesViewDTO>>(await response.Content.ReadAsStringAsync());
+            if (temperatureInfo is null) return null;
+            return temperatureInfo;
+        }
+
+    }
+}
